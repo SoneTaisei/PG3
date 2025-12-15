7da4292460879d39e1739144d019dfe849c8d069
SoneTaisei <k024g0009@g.neec.ac.jp>
2025-12-15 12:12:49 +0900

    昇順と降順の修正。コードの簡略化。

--- 01_02/main.cpp
+++ 01_02/main.cpp
@@ -5,9 +5,9 @@
 #include <vector>
 #include <string>
 
-int main() {
+int main () {
 	// 文字コードをUTF-8にする
-	SetConsoleOutputCP(65001);
+	SetConsoleOutputCP (65001);
 
 	// 配列を作る
 	std::vector<std::string> nameList = {
@@ -26,32 +26,14 @@ int main() {
 		"k024g0093@g.neec.ac.jp","k024g0024@g.neec.ac.jp",
 	};
 
-	std::sort(nameList.begin(), nameList.end(), [](const std::string &a, const std::string &b) {
-		// 文字列aから学籍番号の部分を抜き出す
-		size_t g_pos = a.find('g');       // 'g'の位置を探す
-		size_t at_pos = a.find('@');      // '@'の位置を探す
-		// 'g'の次の文字から、'@'の手前までの部分を切り出す
-		std::string id_str_a = a.substr(g_pos + 1, at_pos - (g_pos + 1));
-		int id_a = std::stoi(id_str_a);     // 文字列を整数に変換
+	// 修正箇所：複雑な切り出しをやめて、単純な辞書順比較に変更
+	std::sort (nameList.begin (), nameList.end (), [](const std::string &a, const std::string &b) {
+		return a < b;
+			   });
 
-
-		std::string id_str_b = b.substr(g_pos + 1, at_pos - (g_pos + 1));
-		int id_b = std::stoi(id_str_b);
-
-		// 整数に変換した学籍番号を比較する
-		if(id_a < id_b) {
-			// aの学籍番号がbより小さいので、aを先に並べる -> trueを返す
-			return true;
-		} else {
-			// aの学籍番号がb以上なので、aを先に並べる必要はない -> falseを返す
-			return false;
-		}
-			  });
-
-
-	// ステップ4: 結果の表示
+	// 結果の表示
 	std::cout << "--- 学籍番号順に表示する ---" << std::endl;
-	for(const std::string &newNameList : nameList) {
+	for (const std::string &newNameList : nameList) {
 		std::cout << newNameList << std::endl;
 	}
 
