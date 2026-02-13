923869358e805c027018349a044578019705c1ee
SoneTaisei <k024g0009@g.neec.ac.jp>
2026-02-13 13:01:38 +0900

    ファイルよっみこみができないバグを直したはず

--- 03_02/Comparator.h
+++ 03_02/Comparator.h
@@ -2,13 +2,13 @@
 template <typename T1, typename T2>
 class Comparator {
 public:
-    // 値を保存しておくための変数
-    T1 val1;
-    T2 val2;
+	// 値を保存しておくための変数
+	T1 val1;
+	T2 val2;
 
-    // コンストラクタ：渡された (10, 20) などを受け取って、変数に入れる
-    Comparator(T1 a, T2 b) : val1(a), val2(b) {}
-    auto Min() {
-        return (val1 < val2) ? val1 : val2;
-    }
+	// コンストラクタ：渡された (10, 20) などを受け取って、変数に入れる
+	Comparator (T1 a, T2 b) : val1 (a), val2 (b) {}
+	auto Min () {
+		return (val1 < val2) ? val1 : val2;
+	}
 };
